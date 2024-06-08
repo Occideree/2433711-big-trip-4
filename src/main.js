e55900a7a8266f670fb01bb7c95de945463a0d2b
Occideree <incorporeall@yandex.com>
2024-06-08 16:18:06 +0500

    Рефакторинг

--- src/main.js
+++ src/main.js
@@ -11,14 +11,14 @@ import OffersApiService from './api/offers-api-service.js';
 import DestinationsApiService from './api/destinations-api-service.js';
 import { render } from './framework/render.js';
 
+const AUTHORIZATION = 'Basic e2c3a4d8e7dfg5df4FDG564Dgfd546g';
+const END_POINT = 'https://21.objects.htmlacademy.pro/big-trip';
+
 export const filterContainer = document.querySelector(
   '.trip-controls__filters'
 );
 const tripInfoContainer = document.querySelector('.trip-main');
 const boardContainer = document.querySelector('.trip-events');
-
-const AUTHORIZATION = 'Basic e2c3a4d8e7dfg5df4FDG564Dgfd546g';
-const END_POINT = 'https://21.objects.htmlacademy.pro/big-trip';
 const destinationsModel = new DestinationsModel({
   destinationsApiService: new DestinationsApiService(END_POINT, AUTHORIZATION),
 });
